69d0b2c6bb3daa4cf43200653680acc74fddfc16
Arthur Coutinho Santos <arthurcoutinho1998@gmail.com>
2021-01-03 21:08:55 -0300

    .

--- src/app/core/services/renda-geral.service.ts
+++ src/app/core/services/renda-geral.service.ts
@@ -2,14 +2,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
-import { FixaAddOrRemovePapelDto } from '../dto/fixa-add-remove.papel.dto';
-import { FixaAlterarPapelDTO } from '../dto/fixa-alterar.papel.dto';
-import { FixaCadastrarPapelDTO } from '../dto/fixa-cadastrar.papel.dto';
-import { FixaDeletarPapelDTO } from '../dto/fixa-deletar.papel.dto';
-import { ConsolidadoRendaFixaModel } from '../model/consolidado-renda-fixa.model';
 import { ConsolidadoRendasModel } from '../model/consolidado-rendas.model';
-import { ConsolidadoSelicModel } from '../model/consolidado-selic.model';
-import { PapelSelicModel } from '../model/papel-selic.model';
 
 @Injectable({
   providedIn: 'root'
