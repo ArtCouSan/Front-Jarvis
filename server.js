e6346c4976ea234e5171e6c80eb4d9e95b1795ed
Arthur Coutinho Santos <arthurcoutinho1998@gmail.com>
2021-01-03 21:47:11 -0300

    .

--- server.js
+++ server.js
@@ -2,14 +2,12 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-const PORT = process.env.PORT || 8080;
-
 app.use(express.static(__dirname + '/dist/front-jarvis'));
 
 app.get('/*', (req, resp) => {
     resp.send(__dirname + '/dist/front-jarvis/index.html');
 });
 
-app.listen(PORT, () => {
+app.listen(process.env.PORT || 5000, () => {
     console.log("Servidor iniciado");
 })
